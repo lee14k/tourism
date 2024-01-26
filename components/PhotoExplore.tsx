33be3ee4347b4baab427ff1fbdc8e307686b58e1
lee14k <kmkovach@mtu.edu>
2024-01-26 17:44:37 -0500

    photo grid

--- components/PhotoExplore.tsx
+++ components/PhotoExplore.tsx
@@ -3,29 +3,32 @@ import { Grid, GridItem } from '@chakra-ui/react';
 
 const PhotoExplore = () => {
     return (
-        <div className="">
+        <div className="grid grid-cols-2">
             <h1>Two Photos Grid</h1>
-            <Grid templateColumns="2fr 1fr" gap={4} className="grid-cols-3">
-                <GridItem colSpan={2}>
-                    <img src="stok.jpg" alt="Photo 1" />
-                </GridItem>
-                <GridItem colSpan={1}>
-                    <img src="stok.jpg" alt="Photo 2" />
-                </GridItem>
-            </Grid>
+            <div>
+            <div className="grid grid-cols-3">
+                <div>
+                    <img src="stok.jpg" alt="Photo 3" />
+                </div>
+                <div  className="col-span-2">
+                    <img src="stok.jpg" alt="Photo 3" />
+                </div>
+            </div>
+            </div>
 
             <h1>Three Photos Grid</h1>
-            <Grid templateColumns="3fr 2fr 1fr" gap={4}>
-                <GridItem colSpan={3}>
+
+            <div className="grid grid-cols-6">
+                <div className="col-span-3">
                     <img src="stok.jpg" alt="Photo 3" />
-                </GridItem>
-                <GridItem colSpan={2}>
-                    <img src="stok.jpg" alt="Photo 4" />
-                </GridItem>
-                <GridItem colSpan={1}>
-                    <img src="stok.jpg" alt="Photo 5" />
-                </GridItem>
-            </Grid>
+                </div>
+                <div  className="col-span-2">
+                    <img src="stok.jpg" alt="Photo 3" />
+                </div>
+                <div  className="col-span-1">
+                    <img src="stok.jpg" alt="Photo 3" />
+                </div>
+            </div>
         </div>
     );
 };
