ee765e36eb03fb596ce82c786e62f36fb62c32ca
lee14k <kmkovach@mtu.edu>
2024-03-14 17:00:37 -0400

    i dont remember what i did

--- components/Transportation.tsx
+++ components/Transportation.tsx
@@ -1,7 +1,7 @@
 export default function Transportation() {
   return (
     <div className="grid grid-cols-2">
-      <div className="transportation">This is Component A</div>
+      <div className="transportation"></div>
       <div>
         <h1></h1>
       </div>
