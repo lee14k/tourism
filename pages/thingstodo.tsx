1177008d9f0a35b8e33912e46569cb148e36e79b
lee14k <kmkovach@mtu.edu>
2024-03-14 01:10:49 -0400

    things to do template

--- pages/thingstodo.tsx
+++ pages/thingstodo.tsx
@@ -1,37 +1,11 @@
-import React, { ComponentType, useState } from 'react';
-import Lodging from '@/components/Lodging';
-import Food from '@/components/Food';
-import Navbar from '@/components/Navbar';
-import Transportation from '@/components/Transportation';
-const ParentComponent: React.FC = () => {
-const [activeComponent, setActiveComponent] = useState<string>('');
-
-const renderComponent = () => {
-    switch (activeComponent) {
-        case 'Food':
-            return <Food />;
-        case 'Lodging':
-            return <Lodging />;
-        case 'Transportation':
-            return <Transportation/>
-
-        default:
-            return <div>Select a component</div>;
-    }
-};
-
+import Navbar from "@/components/Navbar";
+import ThingsFill from "@/components/ThingsFill";
+const ThingsToDo = () => {
   return (
     <div>
-        <Navbar/>
-        <div >
-      <button onClick={() => setActiveComponent('Food')}>Load Component A</button>
-      </div>
-      <button onClick={() => setActiveComponent('Lodging')}>Load Component B</button>
-      <button onClick={() => setActiveComponent('Transportation')}>Load Component C</button>
-
-      <div>{renderComponent()}</div>
+      <Navbar />
+      <ThingsFill />
     </div>
   );
-};
-
-export default ParentComponent;
+}
+export default ThingsToDo;
