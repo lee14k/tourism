ebe8367cb5df9a31e334ea555fb239bd0fde8d19
lee14k <kmkovach@mtu.edu>
2024-04-08 17:44:22 -0400

    now i'm done

--- components/ExploreBox.tsx
+++ components/ExploreBox.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Slider from "./Slider";
 const ExploreBox: React.FC = () => {
   return (
-    <div className="explore-box grid grid-cols-2">
+    <div className="explore-box flex flex-col">
       <div className="text-column flex flex-col justify-center items-center">
         <span className="text-6xl">Come visit the beautiful tropics of Taniti</span>
        
